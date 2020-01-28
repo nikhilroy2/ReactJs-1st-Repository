2778937abd94ae49a59094494e1b3b0eee852c1f
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2020-01-28 09:57:31 +0600

    start first reactjs

---
+++ precache-manifest.3e2f31e0d391e3c292294dac17b8031a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a0d3e60e4e18812763bc86e1d52c6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5435dbda0f4348fa589e",
+    "url": "./static/css/main.7fc50ed4.chunk.css"
+  },
+  {
+    "revision": "4b259a08ed2729258710",
+    "url": "./static/js/2.140c0335.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.140c0335.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5435dbda0f4348fa589e",
+    "url": "./static/js/main.f7a42671.chunk.js"
+  },
+  {
+    "revision": "1d1feb9ac959fa64e717",
+    "url": "./static/js/runtime-main.33d7aa7f.js"
+  },
+  {
+    "revision": "8e26f22094a11f6a689d8302dc30782c",
+    "url": "./static/media/reactjs.8e26f220.svg"
+  }
+]);
